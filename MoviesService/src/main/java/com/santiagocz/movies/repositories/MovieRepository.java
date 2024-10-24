f0f1aa5b29d8e8bc38836e0439a4dcb24c039dad
SantiagoCz <czarnysantiago@gmail.com>
2024-10-24 01:42:45 -0300

    add Movie repository

---
+++ MoviesService/src/main/java/com/santiagocz/movies/repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.santiagocz.movies.repositories;
+
+import com.santiagocz.movies.entities.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+}
