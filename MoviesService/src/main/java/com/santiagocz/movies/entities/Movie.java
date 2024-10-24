6f76d23268d28ee3f5eda574a626bb5d879117cf
SantiagoCz <czarnysantiago@gmail.com>
2024-10-24 01:38:06 -0300

    add Movie entity

--- MoviesService/src/main/java/com/santiagocz/movies/entities/Movie.java
+++ MoviesService/src/main/java/com/santiagocz/movies/entities/Movie.java
@@ -1,11 +1,14 @@
 package com.santiagocz.movies.entities;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
+import jakarta.validation.constraints.*;
+import java.time.LocalDate;
 
 @Builder
 @Data
@@ -15,4 +18,26 @@ import lombok.NoArgsConstructor;
 @Table(name = "movie")
 public class Movie {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Image URL is mandatory")
+    @Size(max = 255, message = "Image URL can't exceed 255 characters")
+    private String image;
+
+    @NotBlank(message = "Title is mandatory")
+    @Size(max = 255, message = "Title can't exceed 255 characters")
+    private String title;
+
+    @NotNull(message = "Creation date is mandatory")
+    private LocalDate creationDate;
+
+    @Min(value = 1, message = "Rating must be at least 1")
+    @Max(value = 5, message = "Rating must be at most 5")
+    private Integer rating;
+
+    @ElementCollection
+    private List<Long> characterIds;
 }
+
