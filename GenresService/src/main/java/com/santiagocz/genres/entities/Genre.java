ef5f85324d8eea480b8554aeeec28cabfa8c4c10
SantiagoCz <134440621+SantiagoCz@users.noreply.github.com>
2024-10-25 10:06:08 -0300

    add Genre entity

---
+++ GenresService/src/main/java/com/santiagocz/genres/entities/Genre.java
@@ -0,0 +1,33 @@
+package com.santiagocz.genres.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "genre")
+public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    private String image;
+
+    @ElementCollection
+    private List<Long> moviesIds;
+
+}
