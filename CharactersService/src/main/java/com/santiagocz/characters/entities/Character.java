01c18b27fa57e857eee71d3826c4cf2c65ef0423
SantiagoCz <134440621+SantiagoCz@users.noreply.github.com>
2024-10-25 10:04:21 -0300

    add Character entity

---
+++ CharactersService/src/main/java/com/santiagocz/characters/entities/Character.java
@@ -0,0 +1,43 @@
+package com.santiagocz.characters.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "character")
+public class Character {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @NotNull(message = "Age is mandatory")
+    @Positive(message = "Age must be a positive number")
+    private int age;
+
+    private String image;
+
+    @NotBlank(message = "Story is mandatory")
+    @Size(max = 255, message = "Story can't exceed 255 characters")
+    private String story;
+
+    @ElementCollection
+    private List<Long> moviesIds;
+
+}
