91f73b3dc9c75788c9494b320430943467d47bd0
SantiagoCz <134440621+SantiagoCz@users.noreply.github.com>
2024-10-27 16:08:08 -0300

    refactor: add import statement for Query annotation in GenreRepository

--- GenresService/src/main/java/com/santiagocz/genres/repositories/GenreRepository.java
+++ GenresService/src/main/java/com/santiagocz/genres/repositories/GenreRepository.java
@@ -2,8 +2,12 @@ package com.santiagocz.genres.repositories;
 
 import com.santiagocz.genres.entities.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
+    @Query("SELECT CASE WHEN COUNT(g) > 0 THEN true ELSE false END FROM Genre g WHERE UPPER(g.name) = UPPER(:name)")
+    boolean existsByName(@Param("name") String name);
 }
