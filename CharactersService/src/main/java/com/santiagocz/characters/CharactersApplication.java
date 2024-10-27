b500ae973c94f80a12089b6588742e7c1ab228e0
SantiagoCz <134440621+SantiagoCz@users.noreply.github.com>
2024-10-27 15:57:58 -0300

    feat: enable discovery and Feign clients in CharactersApplication

--- CharactersService/src/main/java/com/santiagocz/characters/CharactersApplication.java
+++ CharactersService/src/main/java/com/santiagocz/characters/CharactersApplication.java
@@ -2,8 +2,12 @@ package com.santiagocz.characters;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class CharactersApplication {
 
 	public static void main(String[] args) {
