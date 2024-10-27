60369fae6f3de07f5a039e95d0e7515478127084
SantiagoCz <134440621+SantiagoCz@users.noreply.github.com>
2024-10-27 16:06:30 -0300

    feat(GenreController): add image upload support, validation improvements, and movie assignment endpoint

--- GenresService/src/main/java/com/santiagocz/genres/controllers/GenreController.java
+++ GenresService/src/main/java/com/santiagocz/genres/controllers/GenreController.java
@@ -1,15 +1,17 @@
 package com.santiagocz.genres.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.santiagocz.genres.classes.Validations;
 import com.santiagocz.genres.entities.Genre;
 import com.santiagocz.genres.services.GenreService;
+import com.santiagocz.genres.services.ImageService;
 import jakarta.transaction.Transactional;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -19,6 +21,12 @@ public class GenreController {
 
     @Autowired
     private GenreService genreService;
+
+    @Autowired
+    private ImageService imageService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @GetMapping("/all")
     public List<Genre> findAll(){
@@ -30,40 +38,65 @@ public class GenreController {
         return genreService.getById(id);
     }
 
+    @Transactional
     @PostMapping("/save")
-    public ResponseEntity<?> save(@RequestBody @Valid Genre request,
-                                  BindingResult result){
-        ResponseEntity<?> validationResponse = Validations.handleValidationErrors(result);
-        if (validationResponse != null) {
-            return validationResponse;
-        }
+    public ResponseEntity<?> save(@RequestParam(value = "image", required = false) MultipartFile image,
+                                  @RequestParam("genre") String genreJson) {
         try {
+            // Convert the received JSON to a Genre object
+            Genre request = objectMapper.readValue(genreJson, Genre.class);
+
+            ResponseEntity<?> validationResponse = Validations.validateFields(request);
+            if (validationResponse != null) {
+                return validationResponse;
+            }
+
             genreService.save(request);
-            return ResponseEntity.ok("Genre has been saved succesfully.");
+
+            if (image != null && !image.isEmpty()) {
+                imageService.saveImage(image, request);
+            }
+
+            return ResponseEntity.ok("Genre has been saved successfully.");
         } catch (Exception e) {
             String message = "An internal server error occurred: " + e.getMessage();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message);
         }
     }
 
-    @PutMapping("/edit/{id}")
-    public ResponseEntity<?> edit(@RequestBody @Valid Genre request,
-                                  @PathVariable Long id,
-                                  BindingResult result) {
-        ResponseEntity<?> validationResponse = Validations.handleValidationErrors(result);
-        if (validationResponse != null) {
-            return validationResponse;
-        }
-
+    @Transactional
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id,
+                                    @RequestParam(value = "image", required = false) MultipartFile image,
+                                    @RequestParam("genre") String genreJson) {
         try {
             // Find the current genre by ID
             Genre genre = genreService.getById(id);
 
-            // Update the genre fields
-            genre.setName(request.getName());
+            // Convert the received JSON to a Genre object
+            Genre request = objectMapper.readValue(genreJson, Genre.class);
 
-            // Save the updated genre
-            genreService.save(genre);
+            ResponseEntity<?> validationResponse = Validations.validateFields(request);
+            if (validationResponse != null) {
+                return validationResponse;
+            }
+
+            // Check if the name has changed
+            if (!genre.getName().equalsIgnoreCase(request.getName())) {
+                // If the name has changed, check if the new name already exists
+                if (genreService.existsByName(request.getName())) {
+                    throw new ResponseStatusException(HttpStatus.CONFLICT, "Genre with this name already exists: " + request.getName());
+                }
+            }
+
+            // Update the genre fields
+            genre.setName(request.getName().toUpperCase());
+
+            genreService.update(genre);
+
+            if (image != null && !image.isEmpty()) {
+                imageService.saveImage(image, genre);
+            }
 
             return ResponseEntity.ok("Genre updated successfully");
 
@@ -72,6 +105,7 @@ public class GenreController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message);
         }
     }
+
 
     @Transactional
     @DeleteMapping("/delete/{id}")
@@ -83,6 +117,25 @@ public class GenreController {
             return ResponseEntity.ok("Genre has been deleted successfully");
 
         } catch (Exception e) {
+            String message = "An internal server error occurred: " + e.getMessage();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message);
+        }
+    }
+
+    @PostMapping("/assign-movie")
+    public ResponseEntity<?> assignMovieToGenres(@RequestParam Long movieId, @RequestParam List<Long> genresIds) {
+        try {
+            // Call the service method to assign the movie to the genres
+            genreService.assignMovieToGenres(movieId, genresIds);
+
+            // Return a successful response
+            return ResponseEntity.ok("The movie has been successfully assigned to the genres.");
+
+        } catch (IllegalArgumentException e) {
+            // Handle invalid argument errors
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        } catch (Exception e) {
+            // Handle other internal server errors
             String message = "An internal server error occurred: " + e.getMessage();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message);
         }
