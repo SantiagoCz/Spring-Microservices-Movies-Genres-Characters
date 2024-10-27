f30770ed89c643245039c2042954d102aef86384
SantiagoCz <134440621+SantiagoCz@users.noreply.github.com>
2024-10-27 15:59:39 -0300

    feat: enhance CharacterService with name validation and movie assignment

--- CharactersService/src/main/java/com/santiagocz/characters/services/CharacterService.java
+++ CharactersService/src/main/java/com/santiagocz/characters/services/CharacterService.java
@@ -15,7 +15,15 @@ public class CharacterService {
     @Autowired
     private CharacterRepository characterRepository;
 
-    public Character save(Character character){
+    public Character save(Character character) {
+        character.setName(character.getName().toUpperCase());
+        if (existsByName(character.getName())) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Character with this name already exists: " + character.getName());
+        }
+        return characterRepository.save(character);
+    }
+
+    public Character update(Character character) {
         return characterRepository.save(character);
     }
 
@@ -30,5 +38,23 @@ public class CharacterService {
     public Character getById(Long id){
         return characterRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Character not found with ID: " + id));
+    }
+
+    public boolean existsByName(String name) {
+        return characterRepository.existsByName(name);
+    }
+
+    // Method to assign a movie to multiple characters
+    public void assignMovieToCharacters(Long movieId, List<Long> charactersIds) {
+        // Iterate over the IDs of the characters to assign them to the movie
+        for (Long characterId : charactersIds) {
+            Character character = getById(characterId);
+
+            // Verify if the character is already assigned to the movie
+            if (!character.getMoviesIds().contains(movieId)) {
+                character.getMoviesIds().add(movieId);
+                characterRepository.save(character);
+            }
+        }
     }
 }
