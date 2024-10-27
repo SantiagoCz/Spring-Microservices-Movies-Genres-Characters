50f7700dc8339da0f300f49560619a21cecba098
SantiagoCz <134440621+SantiagoCz@users.noreply.github.com>
2024-10-27 16:27:56 -0300

    feat: add field validation method to Validations class

--- GenresService/src/main/java/com/santiagocz/genres/classes/Validations.java
+++ GenresService/src/main/java/com/santiagocz/genres/classes/Validations.java
@@ -1,10 +1,15 @@
 package com.santiagocz.genres.classes;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 
 import java.util.List;
+import java.util.Set;
 
 public class Validations {
 
@@ -20,5 +25,21 @@ public class Validations {
             return ResponseEntity.badRequest().body(errorMessage.toString());
         }
         return null; // No validation errors
+    }
+
+    public static <T> ResponseEntity<?> validateFields(T object) {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<T>> violations = validator.validate(object);
+        if (!violations.isEmpty()) {
+            // If there are validation errors, build a detailed error message
+            StringBuilder errorMessage = new StringBuilder("Validation error in the following fields:\n");
+            for (ConstraintViolation<T> violation : violations) {
+                errorMessage.append("- ").append(violation.getPropertyPath()).append(": ").append(violation.getMessage()).append("\n");
+            }
+            // Return an error response with validation details
+            return ResponseEntity.badRequest().body(errorMessage.toString());
+        }
+        return null;
     }
 }
