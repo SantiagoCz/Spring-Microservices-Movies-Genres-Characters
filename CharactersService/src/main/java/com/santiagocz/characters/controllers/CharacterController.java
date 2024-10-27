4b24b1c1217c70507303d2f0ae3149e9aa456134
SantiagoCz <134440621+SantiagoCz@users.noreply.github.com>
2024-10-27 15:54:07 -0300

    feat(CharacterController): add image upload support, validation improvements, and movie assignment endpoint

--- CharactersService/src/main/java/com/santiagocz/characters/controllers/CharacterController.java
+++ CharactersService/src/main/java/com/santiagocz/characters/controllers/CharacterController.java
@@ -1,8 +1,10 @@
 package com.santiagocz.characters.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.santiagocz.characters.classes.Validations;
 import com.santiagocz.characters.entities.Character;
 import com.santiagocz.characters.services.CharacterService;
+import com.santiagocz.characters.services.ImageService;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -19,6 +23,12 @@ public class CharacterController {
 
     @Autowired
     private CharacterService characterService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private ImageService imageService;
 
     @GetMapping("/all")
     public List<Character> findAll(){
@@ -30,45 +40,69 @@ public class CharacterController {
         return characterService.getById(id);
     }
 
+    @Transactional
     @PostMapping("/save")
-    public ResponseEntity<?> save(@RequestBody @Valid Character request,
-                                  BindingResult result){
-        ResponseEntity<?> validationResponse = Validations.handleValidationErrors(result);
-        if (validationResponse != null) {
-            return validationResponse;
-        }
+    public ResponseEntity<?> save(@RequestParam(value = "image", required = false) MultipartFile image,
+                                  @RequestParam("character") String characterJson) {
         try {
+            // Convert the received JSON to a Character object
+            Character request = objectMapper.readValue(characterJson, Character.class);
+
+            ResponseEntity<?> validationResponse = Validations.validateFields(request);
+            if (validationResponse != null) {
+                return validationResponse;
+            }
+
             characterService.save(request);
-            return ResponseEntity.ok("Genre has been saved succesfully.");
+
+            if (image != null && !image.isEmpty()) {
+                imageService.saveImage(image, request);
+            }
+
+            return ResponseEntity.ok("Character has been saved successfully.");
         } catch (Exception e) {
             String message = "An internal server error occurred: " + e.getMessage();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message);
         }
     }
 
-    @PutMapping("/edit/{id}")
-    public ResponseEntity<?> edit(@RequestBody @Valid Character request,
-                                  @PathVariable Long id,
-                                  BindingResult result) {
-        ResponseEntity<?> validationResponse = Validations.handleValidationErrors(result);
-        if (validationResponse != null) {
-            return validationResponse;
-        }
-
+    @Transactional
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id,
+                                    @RequestParam(value = "image", required = false) MultipartFile image,
+                                    @RequestParam("character") String characterJson) {
         try {
             // Find the current character by ID
             Character character = characterService.getById(id);
 
+            // Convert the received JSON to a Character object
+            Character request = objectMapper.readValue(characterJson, Character.class);
+
+            ResponseEntity<?> validationResponse = Validations.validateFields(request);
+            if (validationResponse != null) {
+                return validationResponse;
+            }
+
+            // Check if the name has changed
+            if (!character.getName().equalsIgnoreCase(request.getName())) {
+                // If the name has changed, check if the new name already exists
+                if (characterService.existsByName(request.getName())) {
+                    throw new ResponseStatusException(HttpStatus.CONFLICT, "Character with this name already exists: " + request.getName());
+                }
+            }
+
             // Update the character fields
-            character.setName(request.getName());
+            character.setName(request.getName().toUpperCase());
             character.setAge(request.getAge());
             character.setStory(request.getStory());
 
-            // Save the updated character
-            characterService.save(character);
+            characterService.update(character);
+
+            if (image != null && !image.isEmpty()) {
+                imageService.saveImage(image, character);
+            }
 
             return ResponseEntity.ok("Character updated successfully");
-
         } catch (Exception e) {
             String message = "An internal server error occurred: " + e.getMessage();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message);
@@ -90,4 +124,22 @@ public class CharacterController {
         }
     }
 
+    @PostMapping("/assign-movie")
+    public ResponseEntity<?> assignMovieToCharacters(@RequestParam Long movieId, @RequestParam List<Long> charactersIds) {
+        try {
+            // Call the service method to assign the movie to the characters
+            characterService.assignMovieToCharacters(movieId, charactersIds);
+
+            // Return a successful response
+            return ResponseEntity.ok("The movie has been successfully assigned to the characters.");
+
+        } catch (IllegalArgumentException e) {
+            // Handle invalid argument errors
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        } catch (Exception e) {
+            // Handle other internal server errors
+            String message = "An internal server error occurred: " + e.getMessage();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message);
+        }
+    }
 }
